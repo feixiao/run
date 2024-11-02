03e32ad77b0c11ecff5cad2d4fa548b6dd2ef8f9
feixiao <feixiao2020@sina.com>
2024-11-02 20:51:50 +0800

    添加脚本测试工具

---
+++ test_sadtalker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export BASE_DIR=/Users/frank/ai
+./sadtalker.sh /Users/frank/forbuild/SadTalker/examples/driven_audio/chinese_news.wav \
+    ${BASE_DIR}/img/cxy.png ${BASE_DIR}/cxy.mp4
